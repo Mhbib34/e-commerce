06c658d92f40261e806c60a4f8556b539290e15a
Mhbib34 <mhabib34official@gmail.com>
2025-04-23 15:33:24 +0700

    Adding middleware at product route unit test product

--- BackEnd/src/routes/product-routes.js
+++ BackEnd/src/routes/product-routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import productController from "../controller/product-controller.js";
+import userAuth from "../middleware/user-auth-middleware.js";
+import { isAdmin } from "../middleware/is-admin.js";
 
 export const productRouter = new express.Router();
 
-productRouter.post("/create", productController.create);
+productRouter.post("/create", userAuth, isAdmin, productController.create);
--- BackEnd/test/product.test.js
+++ BackEnd/test/product.test.js
@@ -1,21 +1,54 @@
 import supertest from "supertest";
-import { removeAllTestCategory, removeAllTestProduct } from "./test.util.js";
+import {
+  removeAllTestIsUser,
+  removeAllTestProduct,
+  removeAllTestUser,
+} from "./test.util.js";
 import { app } from "../src/app/app.js";
 
 describe("POST /api/product/create", () => {
+  jest.setTimeout(10000);
   afterEach(async () => {
     await removeAllTestProduct();
-    await removeAllTestCategory();
+    await removeAllTestUser();
+    await removeAllTestIsUser();
+  });
+
+  let tokenAdmin;
+  let tokenUser;
+  beforeEach(async () => {
+    const isAdmin = await supertest(app).post("/api/user/register").send({
+      username: "test",
+      password: "rahasia",
+      name: "test",
+      email: "test@gmail.com",
+      role: "ADMIN",
+    });
+
+    const isUser = await supertest(app).post("/api/user/register").send({
+      username: "test user",
+      password: "rahasia user",
+      name: "test user",
+      email: "testuser@gmail.com",
+    });
+
+    tokenAdmin = isAdmin.body.token;
+    tokenUser = isUser.body.token;
+    console.log(tokenAdmin);
+    console.log(tokenUser);
   });
 
   it("should can create new product", async () => {
-    const result = await supertest(app).post("/api/product/create").send({
-      name: "test product",
-      description: "test product",
-      price: 100,
-      stock: 100,
-      categoryName: "test category",
-    });
+    const result = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=${tokenAdmin}`])
+      .send({
+        name: "test product",
+        description: "test product",
+        price: 100,
+        stock: 100,
+        categoryName: "test category",
+      });
 
     console.log(result.body);
     expect(result.status).toBe(201);
@@ -26,25 +59,31 @@ describe("POST /api/product/create", () => {
   });
 
   it("should reject create product if request is invalid", async () => {
-    const result = await supertest(app).post("/api/product/create").send({
-      name: "",
-      description: "",
-      price: 100,
-      stock: 100,
-      categoryName: "",
-    });
+    const result = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=${tokenAdmin}`])
+      .send({
+        name: "",
+        description: "",
+        price: 100,
+        stock: 100,
+        categoryName: "",
+      });
 
     console.log(result.body);
     expect(result.status).toBe(400);
   });
   it("should reject if product name already created", async () => {
-    let result = await supertest(app).post("/api/product/create").send({
-      name: "test product",
-      description: "test product",
-      price: 100,
-      stock: 100,
-      categoryName: "test category",
-    });
+    let result = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=${tokenAdmin}`])
+      .send({
+        name: "test product",
+        description: "test product",
+        price: 100,
+        stock: 100,
+        categoryName: "test category",
+      });
     console.log(result.body);
 
     expect(result.status).toBe(201);
@@ -53,16 +92,50 @@ describe("POST /api/product/create", () => {
     expect(result.body.product.price).toBe(100);
     expect(result.body.product.stock).toBe(100);
 
-    result = await supertest(app).post("/api/product/create").send({
-      name: "test product",
-      description: "test product",
-      price: 100,
-      stock: 100,
-      categoryName: "test category",
-    });
+    result = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=${tokenAdmin}`])
+      .send({
+        name: "test product",
+        description: "test product",
+        price: 100,
+        stock: 100,
+        categoryName: "test category",
+      });
 
     console.log(result.body);
     expect(result.status).toBe(400);
     expect(result.body.errors).toBeDefined();
+  });
+  it("should reject create new product if token is not valid", async () => {
+    const result = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=dadasd`])
+      .send({
+        name: "test product",
+        description: "test product",
+        price: 100,
+        stock: 100,
+        categoryName: "test category",
+      });
+
+    console.log(result.body);
+    expect(result.status).toBe(401);
+  });
+
+  it("should reject create new product if is not admin", async () => {
+    const result = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=${tokenUser}`])
+      .send({
+        name: "test product",
+        description: "test product",
+        price: 100,
+        stock: 100,
+        categoryName: "test category",
+      });
+
+    console.log(result.body);
+    expect(result.status).toBe(403);
   });
 });
