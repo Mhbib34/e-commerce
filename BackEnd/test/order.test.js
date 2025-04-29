52c3ef8c64b6d15febbdb61940e8314c496be3e0
Mhbib34 <mhabib34official@gmail.com>
2025-04-29 14:02:24 +0700

    Adding unit test get order by orderId API

--- BackEnd/test/order.test.js
+++ BackEnd/test/order.test.js
@@ -189,3 +189,105 @@ describe("GET /api/order/get", () => {
     expect(result.status).toBe(401);
   });
 });
+
+describe("GET /api/order/get/:id", () => {
+  let productId;
+  let token;
+  let userId;
+  let userCardEmpty;
+  let orderId;
+  beforeEach(async () => {
+    const result = await supertest(app).post("/api/user/register").send({
+      username: "test",
+      password: "rahasia",
+      name: "test",
+      email: "test@gmail.com",
+      role: "ADMIN",
+    });
+
+    const userCart = await supertest(app).post("/api/user/register").send({
+      username: "test empty",
+      password: "rahasia empty",
+      name: "test empty",
+      email: "testempty@gmail.com",
+      role: "ADMIN",
+    });
+    console.log(result.body);
+
+    token = result.body.token;
+    userId = result.body.user.id;
+    userCardEmpty = userCart.body.token;
+
+    const product = await supertest(app)
+      .post("/api/product/create")
+      .set("Cookie", [`token=${token}`])
+      .send({
+        name: "test product",
+        description: "test product",
+        price: 100,
+        stock: 100,
+        categoryName: "test category",
+      });
+
+    productId = product.body.product.id;
+
+    await supertest(app)
+      .post("/api/cart-item/add-to-cart")
+      .set("Cookie", [`token=${token}`])
+      .send({
+        productId: productId,
+        quantity: 2,
+      });
+
+    await supertest(app)
+      .post("/api/cart-item/add-to-cart")
+      .set("Cookie", [`token=${userCardEmpty}`])
+      .send({
+        productId: productId,
+        quantity: 2,
+      });
+
+    const order = await supertest(app)
+      .post("/api/order/create")
+      .set("Cookie", [`token=${token}`]);
+
+    orderId = order.body.order.id;
+    console.log(order.body);
+
+    console.log(token);
+    console.log(productId);
+  });
+  afterEach(async () => {
+    await removeAllTestUserOrder(userId);
+    await removeAllTestProduct();
+    await removeAllTestCategory();
+    await removeAllTestUserCartEmpty();
+  });
+
+  it("should can get order by id", async () => {
+    const result = await supertest(app)
+      .get(`/api/order/get/${orderId}`)
+      .set("Cookie", [`token=${token}`]);
+
+    console.log(result.body);
+    expect(result.status).toBe(200);
+  });
+
+  it("should reject get order by id if orderId is not found", async () => {
+    const result = await supertest(app)
+      .get(`/api/order/get/asdasdasdas`)
+      .set("Cookie", [`token=${token}`]);
+
+    console.log(result.body);
+    expect(result.status).toBe(404);
+  });
+
+  it("should reject get order by id if token is not valid", async () => {
+    const result = await supertest(app)
+      .get(`/api/order/get/${orderId}`)
+      .set("Cookie", [`token=asdasd`]);
+
+    console.log(result.body);
+    expect(result.status).toBe(401);
+  });
+});
