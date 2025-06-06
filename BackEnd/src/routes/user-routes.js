2694cb8dff60e1a063034e6b028f5424c447f5de
Mhbib34 <mhabib34official@gmail.com>
2025-06-06 21:42:34 +0700

    Make api reset password otp and reset password no need to login again

--- BackEnd/src/routes/user-routes.js
+++ BackEnd/src/routes/user-routes.js
@@ -12,7 +12,6 @@ userRouter.post("/send-verify-otp", userAuth, userController.verifyOtp);
 userRouter.post("/verify-email", userAuth, userController.verifyEmail);
 userRouter.post(
   "/send-reset-password-otp",
-  userAuth,
   userController.sendresetPasswordOtp
 );
-userRouter.post("/reset-password", userAuth, userController.resetPassword);
+userRouter.post("/reset-password", userController.resetPassword);
