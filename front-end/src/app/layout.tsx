541da29f9f320338a2ac5cbba53b498297ed9f94
Mhbib34 <mhabib34official@gmail.com>
2025-07-05 21:19:34 +0700

    style : changes layout main and rootlayout

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,8 +1,30 @@
 // app/layout.tsx
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+	display: "swap",
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+	display: "swap",
+});
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return <>{children}</>;
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				suppressHydrationWarning
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
